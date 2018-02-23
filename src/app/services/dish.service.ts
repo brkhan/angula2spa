cbf39b9fc24f89d2347784dd4c451167ee85c650
Bilal Khan <bilal.khan@ofgem.gov.uk>
2018-02-23 01:20:24 +0000

    AngularSpaPart1

---
+++ src/app/services/dish.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
+
+@Injectable()
+export class DishService {
+
+  constructor() { }
+
+  getDishes() : Dish[] {
+    return DISHES;
+  }
+
+  getDish(id: number): Dish {
+    return DISHES.filter((a) => a.id === id)[0];
+  }
+  
+  getFeaturedDish() : Dish {
+      return DISHES.filter((a) => a.featured === true)[0];
+  }
+
+}
